4d13d46a57a8722157d06f0a4c9002c7ea33eb21
madgraycat <58690780+madgraycat@users.noreply.github.com>
2020-05-25 12:39:19 +0400

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,10 @@
+﻿#!/bin/bash
+ echo "Hello"
+ let N=3; # количество создаваемых файлов
+ for  ((i=1; i <= N; i++))
+  do
+  base64 /dev/urandom | head -c 10000 > file[$i].txt
+  rsync -avz file[$i].txt gato@192.168.1.152:/tmp/files
+ done
+ ssh gato@192.168.1.152 find /tmp/files -type f -mtime +7 | xargs rm -rf
+
